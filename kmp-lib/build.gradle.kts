cae0b588bd06b36ec55279a1ac40a911ba6ff73b
kirich1409 <kirill@androidbroadcast.dev>
2024-10-06 14:06:47 +0300

    Fix KMP publishing sample

--- kmp-lib/build.gradle.kts
+++ kmp-lib/build.gradle.kts
@@ -56,8 +56,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     // Раздел publishing переходит в настройку kotlin.androidTarget
